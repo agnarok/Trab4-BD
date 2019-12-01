0cba749f165d0d9174c60335a0caf5d30f8b8cf3
Agnaldo <abj@icomp.ufam.edu.br>
2019-11-30 22:43:01 -0400

    Add category to database

--- database.sql
+++ database.sql
@@ -25,3 +25,51 @@ create table similars
 
 alter table similars
     owner to postgres;
+
+create table categories
+(
+    id   integer not null
+        constraint categories_pk
+            primary key,
+    name varchar
+);
+
+alter table categories
+    owner to postgres;
+
+create table product_category
+(
+    product_asin varchar
+        constraint product_category_products_asin_fk
+            references products,
+    category_id  integer
+        constraint product_category_categories_id_fk
+            references categories
+);
+
+alter table product_category
+    owner to postgres;
+
+create unique index product_category_product_asin_category_id_uindex
+    on product_category (product_asin, category_id);
+
+create table reviews
+(
+    id           serial not null
+        constraint reviews_pk
+            primary key,
+    date         date,
+    customer     varchar,
+    rating       integer,
+    votes        integer,
+    helpful      integer,
+    product_asin varchar
+        constraint reviews_products_asin_fk
+            references products
+);
+
+alter table reviews
+    owner to postgres;
+
+create unique index reviews_id_uindex
+    on reviews (id);
